0963cda531316606a14f9dce1599de5a1a6e89fd
Craig Archibald <carchibald@riis.com>
2023-01-19 17:26:44 -0500

    Removed redundant files and merged duplicates

--- app/src/main/java/com/riis/cameraapp/MApplication.kt
+++ app/src/main/java/com/riis/cameraapp/MApplication.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import com.secneo.sdk.Helper
 
 class MApplication: Application() {
-
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         Helper.install(this)
